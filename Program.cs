bd48b8e52b2df33d4c0a061f55250965a187230b
Sushkoo <szilagyidominik005@gmail.com>
2023-05-31 09:32:57 +0200

    1.push

--- Program.cs
+++ Program.cs
@@ -12,7 +12,43 @@ namespace doga
         static void Main(string[] args)
         {
             //1.feladat
-           
+            StreamWriter cw = new StreamWriter("felfedezesek.csv");
+
+            List<ListaElem> lista = new List<ListaElem>();
+            
+
+            bool EndOfStream = true;
+            while (!EndOfStream)
+            {
+                lista.Add();
+            }
+
+            //3.feladat
+            int kemiaiElemek = lista.Count();
+            Console.WriteLine($"3.feladat: Elemek száma:{kemiaiElemek}");
+
+            //4.feladat
+            int okoriFelfedezesek = lista.Where(x=> x="Ókor").Count();
+            Console.WriteLine($"4.feladat: Felfedezések száma az ókorban: {okoriFelfedezesek}");
+
+            //5.feladat
+            Console.WriteLine("Kérek egy vegyjelet: ");
+            foreach (var i in lista)
+            {
+
+            }
+           string vegyjel=Console.ReadLine();
+           string keresettElem = lista.ForEach(x => x = vegyjel);
+
+
+            //6.feladat
+
+            //7.feladat
+
+            //8.feladat
+
+            
+
         }
     }
 }
